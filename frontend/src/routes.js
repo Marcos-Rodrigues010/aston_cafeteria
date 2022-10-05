08dd31ac316de35d39c2f3fe4f38d376fa3e4bfa
Marcos <marcos.rodrigues@youxgroup.com.br>
2022-10-05 12:47:28 -0300

    Remoção de arquivos não utilizados

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import RequireAuth from './components/RequireAuth/RequireAuth';
 import Signup from './pages/Signup';
 import Product from './pages/Product';
 import Payment from './pages/Payment';
 import Favorites from './pages/Favorites';
 import Cart from './pages/Cart';
 import Orders from './pages/Orders';
-import NotFound from './pages/NotFound';
 
 
 export default () => {
@@ -22,7 +20,6 @@ export default () => {
             <Route path='/favorites' element={<Favorites />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='/orders' element={<Orders />} />
-            <Route path='*' element={<NotFound />} />
         </Routes>
     );
 };
